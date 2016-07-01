54d289f426dfa863c9751b0cbf1a9a9048234fe4
Anton <anton.bil@hz.nl>
2016-07-01 11:13:09 +0200

    Uitbreiden query naar Light Context EN Project

--- js/veExtenderConstants.js
+++ js/veExtenderConstants.js
@@ -1,11 +1,11 @@
 function veExtenderQueries(){
   return {
     //used in invoegen-menu
-    linkpages:'[[Category:Light Context]]',//todo: naamgeving; zet links ervoor, in plaats van eracher
+    linkpages:'[[Category:Light Context||Project]]',//todo: naamgeving; zet links ervoor, in plaats van eracher
     linkwebsites:'[[Category:Resource Description]] [[Hyperlink::+]]',
     linkreferences:'[[Category:Resource Description]] [[:+]] OR [[Category:Resource Description]] [[File:+]] [[Pagename::~*.pdf||~*.doc||~*.docx||~*.ppt||~*.pptx||~*.odt||~*.odc||~*.odp||~*.odg||~*.txt]]',//
     //used in resource-menu
-    resourcepages:'[[Category:Light Context]]',
+    resourcepages:'[[Category:Light Context||Project]]',
     resourcehyperlinks:'[[Category:Resource Description]] [[:+]]',
     resourceuploadables:'[[Category:Resource Description]] [[File:+]]'
   };
