a23a2fb7cd704d5cd2cba8d9a16821f264f83df5
Anton <anton.bil@hz.nl>
2016-09-02 09:52:43 +0200

    get tool-item using code instead of absolute number

--- js/veExtender.js
+++ js/veExtender.js
@@ -1,6 +1,19 @@
   //redefine what tools will be initially available in the insert-menu
 function redefineMenu(){
+  var tools=ve.ui.toolFactory.bindings.register;
   var force=ve.ui.toolFactory.bindings.register[6].context.forceExpand;
+  for (var j=0;j<tools.length;j++){
+    var force1=ve.ui.toolFactory.bindings.register[6].context.forceExpand;
+    if (force1 == null) {
+	// do something 
+    } else{
+      for (var k=0;k<force1.length;k++){
+	if (force1[k]=="media"){
+	  force=force1;
+	}
+      }
+    }
+  }
   //remove transclusion from neu if available
   for (var i=0;i<force.length;i++)
     if (force[i]=="transclusion"){
