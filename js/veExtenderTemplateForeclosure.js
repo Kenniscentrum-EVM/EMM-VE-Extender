874d3fa636206918017782927f6dac9f4658afee
NickBB <devnickp@gmail.com>
2016-10-12 13:52:15 +0200

    + Commentaar toegevoegd

--- js/veExtenderTemplateForeclosure.js
+++ js/veExtenderTemplateForeclosure.js
@@ -9,17 +9,25 @@
 var VEETemplateForclosure = function(protectedTypes) {
     this.protectedTypes = protectedTypes;
 
+    //de methode die over de verwijdering van elmenten gaat (terug te vinden in \mediawiki\extensions\VisualEditor\lib\ve\src\dm\ve.dm.Transaction.js)
     var base = ve.dm.Transaction.prototype.addSafeRemoveOps;
     ve.dm.Transaction.prototype.addSafeRemoveOps = function(doc, removeStart, removeEnd, removeMetadata)
     {
+        // x is de offset van het punt in het document waar iets verwijdert moet worden.
         for(x = removeStart; x < removeEnd; x++){
+            // heeft het document op deze offset een element met een type? zo ja, dan is het een node. Zo niet, dan is het gewoon tekst.
             if(doc.data.getType(x) != null)
             {
-                var node = doc.getDocumentNode().getNodeFromOffset(x);
+                // verkrijg het node object vanuit de offset.
+                var node = doc.getDocumentNode().getNodeFromOffset(x)
                 console.log(node.type);
+                // loop door de array met beschermde types
                 for(var i = 0; i < protectedTypes.length; i++)
+                    // is het type gelijk aan een type in de lijst
                     if(node.type == protectedTypes[i]) {
+                        // maak het element niet meer deletebaar.
                         ve.dm.nodeFactory.registry[doc.data.getType(x)].static.isDeletable = false;
+                        // tijdelijke feedback.
                         alert('waarschuwing, u probeert een systeemtemplate te verwijderen, dit is niet toegestaan!');
                     }
             }
