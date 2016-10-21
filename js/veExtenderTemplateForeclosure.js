dfb164b3cc05f1fc2c42b62d9c576c79248d0c52
NickBB <devnickp@gmail.com>
2016-10-21 14:08:36 +0200

    + Got the tranasclusion protection semi-working now.

--- js/veExtenderTemplateForeclosure.js
+++ js/veExtenderTemplateForeclosure.js
@@ -9,123 +9,94 @@
 var VEETemplateForclosure = function(protectedTypes) {
 
     evaluateTransclusions();
-    var deleteJobs = [];
-
+    var protectedTemplates = {};
     //de methode die over de verwijdering van elmenten gaat (terug te vinden in \mediawiki\extensions\VisualEditor\lib\ve\src\dm\ve.dm.Transaction.js)
     var base = ve.dm.Transaction.prototype.addSafeRemoveOps;
     ve.dm.Transaction.prototype.addSafeRemoveOps = function(doc, removeStart, removeEnd, removeMetadata)
     {
-        var hasTemplate = false;
-        var _lock = false;
-        var deferred = $.Deferred();
-        var _id;
-        var ref = this;
-
         // x is de offset van het punt in het document waar iets verwijdert moet worden.
-        for(x = removeStart; x < removeEnd; x++){
-            //console.log("begin: " + removeStart + " | " + x + " / " + removeEnd + " | end: " + removeEnd);
-            // heeft het document op deze offset een element met een type? zo ja, dan is het een node. Zo niet, dan is het gewoon tekst.
-            if(doc.data.getType(x) != null) {
-                // verkrijg het node object vanuit de offset.
-                var node = doc.getDocumentNode().getNodeFromOffset(x);
-                //console.log(node.type);
-                // loop door de array met beschermde types
-                    // is het type gelijk aan een type in de lijst
-                console.log(node);
+        return removeRange(doc, removeStart, removeEnd, removeMetadata, this);
+    };
 
-                    if (node.type == 'mwTransclusionInline' || node.type == 'mwTransclusionBlock') {
-                        var target;
-                        if(node.type == 'mwTransclusionBlock')
-                        {
-                            for(var z = 0; node.element.attributes.mw.parts.length; z++)
-                                if(typeof node.element.attributes.mw.parts[z] === "object")
-                                {
-                                    //console.log(node.element.attributes.mw.parts[z]);
-                                    target = node.element.attributes.mw.parts[z].template.target.href.split("./").pop();
-                                    break;
-                                }
-                        }
-                        else
-                            target = node.element.attributes.mw.parts[0].template.target.href.split("./").pop();
 
-                        hasTemplate = true;
-                        _lock = true;
-                        _id = window.setInterval(poll_lock, 50);
-
-                        // maak het element niet meer deletebaar.
-                        mw.loader.using('mediawiki.api', function () {
-                                (new mw.Api()).get({
-                                    action: 'query',
-                                    prop: "categories",
-                                    titles: target,
-                                    formatversion: 2
-                                    //titles: 'Template:Cite'
-                                }).done(function (data) {
-                                    console.log(data);
-                                    var page = data.query.pages[0]; // we're only asking for a single page.
-                                    for(var y = 0; y < page.categories.length; y++)
-                                        if(page.categories[y].title.split(":").pop() == "System") {
-                                            console.log("we're going to protect this template!");
-                                            ve.dm.nodeFactory.registry[doc.data.getType(x)].static.isDeletable = false;
-                                        }
-                                    _lock = false;
-
-                                });
-                            }
-                        );
-                    }
-            }
-        }
-        if(!hasTemplate)
-            return base.call(this, doc, removeStart, removeEnd, removeMetadata);
-        else
-            return 0;
-
-        function poll_lock()
+    function evaluateTransclusions() {
+        var nodes = ve.init.target.getSurface().getModel().getDocument().getDocumentNode();
+        var transclusions = getTransclusions(nodes);
+        //todo somehow check if the async operations are complete
+        for(var i = 0; i < transclusions.length; i++)
         {
-            //console.log(_lock);
-            if(_lock == false)
-            {
-                window.clearInterval(_id);
-                console.log("we're going to destroy");
-                //return base.call(this, doc, removeStart, removeEnd, removeMetadata);
-            }
+            var templateName = getTemplate(transclusions[i]);
+            //todo this may have to be wrapped into a closure.
+            mw.loader.using('mediawiki.api', function () {
+                    (new mw.Api()).get({
+                        action: 'query',
+                        prop: "categories",
+                        titles: templateName,
+                        formatversion: 2
+                    }).done(function (data) {
+                        var page = data.query.pages[0]; // we're only asking for a single page.
+                        for(var y = 0; y < page.categories.length; y++) {
+                            if (page.categories[y].title.split(":").pop() == "System") {
+                                protectedTemplates[templateName] = true;
+                            }
+                        }
+                    });
+                }
+            );
         }
-
     }
 
 
-    function evaluateTransclusions()
+    function removeRange(doc, removeStart, removeEnd, removeMetadata, thisContext)
     {
-        var nodes = ve.init.target.getSurface().getModel().getDocument().getDocumentNode().children;
-        var transclusions = [];
-
-        console.log(getTransclusions(nodes));
-
+        var x;
+        var protect = false;
+        for(x = removeStart; x < removeEnd; x++){console.log("offset = " + x);
+            var node = doc.getDocumentNode().getNodeFromOffset(x);
+            console.log("node: ", node);
+            if(node.type != null)
+                if(node.type == 'mwTransclusionBlock' && protectedTemplates[getTemplate(node)] != null)
+                {
+                    console.log("I'm going to protect this node.");
+                    ve.dm.nodeFactory.registry[doc.data.getType(x)].static.isDeletable = false;
+                    protect = true;
+                    //check if we're incremeting correctly?
+                    removeRange(doc, x + 1, removeEnd, removeMetadata, thisContext);
+                    break;
+                }
+        }
+        var returnValue; //todo range -1?
+        if(protect) { //todo perhaps this isn't needed in a proper implementation
+            returnValue = base.call(thisContext, doc, removeStart, x, removeMetadata);
+            ve.dm.nodeFactory.registry[doc.data.getType(x)].static.isDeletable = true;
+        }
+        else
+        {
+            returnValue = base.call(thisContext, doc, removeStart, x, removeMetadata);
+        }
+        return returnValue;
     }
 
     function getTransclusions(node)
     {
-
-        //we only need the transclusionnodes, they are always at the second level
-        console.log("hallo");
-        var leaves = [];
-        if(node.children == null)
-        {
-            leaves.push(node);
-            return leaves;
-        }
+        var transclusions = [];
+        //todo add inline transclusion grabber?
         for(var i = 0; i < node.children.length; i++)
-        {
-            var childLeaves = getTransclusions(node.children[i]);
-            if(childLeaves.children != null)
-                leaves = leaves.concat(childLeaves[i]);
-            else
-                leaves.push(node.children[i]);
+            if(node.children[i].type == 'mwTransclusionBlock')
+                transclusions.push(node.children[i]);
+        return transclusions;
+    }
+
+    function getTemplate(node)
+    {
+        if(node.type == 'mwTransclusionBlock') {
+            for (var z = 0; node.element.attributes.mw.parts.length; z++) {
+                if (typeof node.element.attributes.mw.parts[z] === "object")
+                    return node.element.attributes.mw.parts[z].template.target.href.split("./").pop();
+            }
+            return null;
         }
-        if(!leaves.length)
-            leaves.push(node);
-        return leaves;
+        return node.element.attributes.mw.parts[0].template.target.href.split("./").pop();
     }
 };
 
