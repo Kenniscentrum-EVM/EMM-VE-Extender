4ce1394b19efb918a7f468121531e0734b5c8c33
Anton <anton.bil@hz.nl>
2017-06-20 16:02:24 +0200

    added plugin for accesscontrol

--- EMMVEExtenderExtension.php
+++ EMMVEExtenderExtension.php
@@ -38,14 +38,87 @@ $wgResourceModules['ext.EMMVEExtension'] = array(
 class EMMVEExtenderExtension
 {
 
+    const EMM_ACCESS_CONTROL = "EMMAccessControl";
+
+    const ACCESSCONTROL_TAG = '<accesscontrol>';
+
+    const ACCESSCONTROL_ENDTAG = '</accesscontrol>';
+
+    const END_TEMPLATE = "}}";
+
     public static function onBeforePageDisplay(OutputPage &$output, Skin &$skin)
     {
         $output->addModules('ext.EMMVEExtension');
+        return true;
+    }
+
+    static function get_all_between($beginning, $end, $string) {
+        $beginningPos = strpos($string, $beginning);
+        $endPos = strpos($string, $end);
+        if ($beginningPos === false || $endPos === false) {
+            return "";
+        }
+
+        return substr($string, $beginningPos, ($endPos + strlen($end)) - $beginningPos);
+
+    }
+
+    static function delete_all_between($beginning, $end, $string) {
+        $textToDelete = self::get_all_between($beginning, $end, $string);
+
+        return str_replace($textToDelete, '', $string);
+    }
+
+    public static function onPageContentSave( WikiPage &$wikiPage, User &$user, Content &$content, &$summary,
+                                              $isMinor, $isWatch, $section, &$flags,  Status&$status
+    ) {
+        // check, if the content model is wikitext to avoid adding wikitext to pages, that doesn't handle wikitext (e.g.
+        // some JavaScript/JSON/CSS pages)
+        if ( $content->getModel() === CONTENT_MODEL_WIKITEXT ) {
+            // getNativeData returns the plain wikitext, which this Content object represent..
+            $data = $content->getNativeData();
+            // check whether text contains field
+            $text="";
+            $pos = strpos($data, self::EMM_ACCESS_CONTROL);
+            if ( $pos>0 ) {//field always part of template, so never starts at position 0
+                $pos+=+strlen(self::EMM_ACCESS_CONTROL);
+                $text = substr($data, $pos, (strpos($data, PHP_EOL, $pos)) - $pos);
+                $text = trim($text);
+                $text = ltrim($text, '=');
+            }
+
+            $textToDelete = self::get_all_between(self::ACCESSCONTROL_TAG, self::ACCESSCONTROL_ENDTAG, $data);
+            wfDebug( 'text='.$text );
+            //only do a page-change if accesscontrol has changed
+            if (strcmp ($text,$textToDelete)!=0) {
+                //remove old tags for accesscontrol, including content inbetween
+                $data = self::delete_all_between(self::ACCESSCONTROL_TAG, self::ACCESSCONTROL_ENDTAG, $data);
+                $data=str_replace(self::ACCESSCONTROL_TAG, '', $data);
+                $data=str_replace(self::ACCESSCONTROL_ENDTAG, '', $data);
+
+                if (strlen($text)>0) {
+
+                    //look for place where text should be inserted; after current template-call
+                    $pos2 = strpos($data, self::END_TEMPLATE, $pos) + 2;
+
+                    // add/remove/replace tags and content
+                    $data = substr_replace($data, self::ACCESSCONTROL_TAG . $text . self::ACCESSCONTROL_ENDTAG, $pos2, 0);
+                    // the new wikitext ahs to be saved as a new Content object (Content doesn't support to replace/add content by itself)
+                }
+                $content = new WikitextContent($data);
+                wfDebug( 'access-control changed to:'.$text );
+            }
+            //otherwise do not change text
+        } else
+            wfDebug( 'access-control not changed' );
+
         return true;
     }
 }
 
 // set Hook so module will be loaded
 $wgHooks['BeforePageDisplay'][] = 'EMMVEExtenderExtension::onBeforePageDisplay';
+$wgHooks['PageContentSave'][] = 'EMMVEExtenderExtension::onPageContentSave';
+
 
 ?>
