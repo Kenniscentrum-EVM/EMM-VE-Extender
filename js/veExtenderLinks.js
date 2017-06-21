fa108e70682123d24913a91814738a0c00cbce07
Anton <anton.bil@hz.nl>
2017-06-21 13:53:05 +0200

    date now added correctly to edit-screen

--- js/veExtenderLinks.js
+++ js/veExtenderLinks.js
@@ -73,22 +73,25 @@ function SPARQLStore() {
                     if (line[oldid]["type"]=="uri"){
                         printouts1[newid]=[{fulltext:self.pageName(value2), fullurl:value2}];
                     } else
-                    if (line[oldid]["type"]=="typed-literal"){
+                    if (line[oldid]["type"]=="typed-literal"||line[oldid]["type"]=="literal"){//
                         if (line[oldid]["datatype"]=="http://www.w3.org/2001/XMLSchema#gYear"){
                             value2=value2+"/01/01";
                             var timestamp=parseInt((new Date(value2).getTime() / 1000).toFixed(0));
                             printouts1[newid]=[{raw:"1/"+value2,timestamp:timestamp.toString()}];
+                            console.log("date2:",printouts1[newid]);
                         } else
                         if (line[oldid]["datatype"]=="http://www.w3.org/2001/XMLSchema#date"){
                             value2=value2.replaceAll("-","/").replace("Z","");
                             if (value2.length<5)value2=value2+"/01/01";
                             var id3=parseInt((new Date(value2).getTime() / 1000).toFixed(0));
                             printouts1[newid]=[{raw:"1/"+value2,timestamp:id3.toString()}];
+                            console.log("date:",printouts1[newid]);
                         } else
+                        if (line[oldid]["type"]=="literal"){
+                            printouts1[newid]=[self.pageName(value2)];
+                            console.log("date3:",printouts1[newid]);
+                        }else
                         printouts1[newid]=[cNum((value2))];
-                    } else
-                    if (line[oldid]["type"]=="literal"){
-                        printouts1[newid]=[self.pageName(value2)];
                     }
                     /*if (newid=="Semantic title"&&!(isString(printouts1[newid][0]))){
                         printouts1[newid][0] ="";
