ce9dd2056a6292940aa89fd6508af1e0d278b1ae
Anton <anton.bil@hz.nl>
2017-11-26 23:43:37 +0100

    update gaph now possible

--- generateSparql.py
+++ generateSparql.py
@@ -1,9 +1,11 @@
 import requests,json
 import os
 import urllib.parse
-import yaml
 
-# todo: check in ve-extender dat lijsten alleen worden opgevraagd met sparql, en update-info etc via ask.....
+import sys
+import yaml
+import pickle
+import datetime
 
 # [[Category:Resource Description]] [[file name::+]] |?Semantic title|?Dct:creator|?Dct:date|?Organization|?Dct:subject|?file name|sort=Semantic title|order=asc|limit=10000
 # hieronder uitgewerkt:
@@ -28,7 +30,7 @@ wiki:Property-3ADct-3Adate-23aux  "2457856.5"^^xsd:double
 wiki:Property-3AFile_name  wiki:Bestand-3A141027agenda_programmagroep22.docx
 wiki:Property-3APagename  "Bestand:141027agenda programmagroep22.docx"
 """
-debug=True
+debug=False
 def codeTitle(prefix,title_,url):
     if debug:
         print(title_)
@@ -61,7 +63,7 @@ def simple(x):
 
 stream = open('parameters.yml', 'r')    # 'document.yaml' contains a single YAML document.
 parameters = yaml.load(stream)['parameters']
-
+method = "PUT"
 
 wikiurl=parameters.get('wikiurl',"http://localhost:5555/wikis/hzportfolio/wiki")
 sparqlport=parameters.get('sparqlport',"5030")
@@ -81,31 +83,8 @@ PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 fh = open("data.ttl",'w')
 fh.write(header)
 
-cmd = """<{0}> 
-  rdf:type  swivt:Subject;
-[3]
-  rdfs:label  "{1}";
-  swivt:page  <{0}>;
-[6]
-[4]
-  wiki:Property-3ADisplay_title_of  "{1}";
-[5]
-  wiki:Property-3ASelf  wiki:{2}
-  .
 
-  """
-
-f = [lambda x: codeTitle("rdf:type  wiki:",x["Category"],True).replace("Categorie", "Category"),
-     lambda y: codeTitle("wiki:Property-3ASupercontext  wiki:",y["Supercontext"],True),
-     lambda y: "wiki:Property-3ASemantic_title  \"" + escapeQuote(y["Semantic title"][0]) + "\"",
-     lambda y: "wiki:Property-3AName  \"" + escapeQuote(y["Semantic title"][0]) + "\""]
-
-query="[[Category:Light Context||Project||Projecten]]|?Semantic title|?Category=Category|?Supercontext|sort=Semantic title|order=asc|limit=10000"
-# look for sparql-result:
-# select ?p ?o where { <http://localhost:5555/wikis/hzportfolio/wiki/index.php/LC_00249> ?p ?o}
-
-
-def doCommand(query,cmd,f):
+def doCommand(query,cmd,f,listdata):
     url = wikiurl + "/api.php?action=ask&query=" + urllib.parse.quote_plus(query) + "&format=json"
     print(url)
     r = requests.get(url)
@@ -113,6 +92,7 @@ def doCommand(query,cmd,f):
     # print(data)
     l = data['query']['results']
     for key,mydict in l.items():
+        listdata.append(key)
         lc=key #url
         self1=encodeUrl(lc)
         label=escapeQuote(l[lc]['displaytitle'])
@@ -135,160 +115,230 @@ def doCommand(query,cmd,f):
 import time
 start_time = time.time()
 
+def saveAllResources():
+    listdata = []
 
-#doCommand(query,cmd,f)
-print("--- %s seconds ---" % (time.time() - start_time))
-query="[[Category:Resource Description]]|?Semantic title|?file name|?hyperlink|?Dct:creator|?Dct:date|?Organization|?Dct:subject|limit=10000"
-cmd="""
-<{0}> 
-  rdf:type  swivt:Subject;
-  rdf:type  wiki:Category-3AResource_Description;
-[3]
-  rdfs:label  "{1}";
-  swivt:page  <{0}>;
-[6]
-[7]
-[4]
-  wiki:Property-3ADisplay_title_of  "{1}";
-[5]
-  wiki:Property-3ASelf  wiki:{2}
-  .
+    cmd = """<{0}> 
+      rdf:type  swivt:Subject;
+    [3]
+      rdfs:label  "{1}";
+      swivt:page  <{0}>;
+    [6]
+    [4]
+      wiki:Property-3ADisplay_title_of  "{1}";
+    [5]
+      wiki:Property-3ASelf  wiki:{2}
+      .
 
-  """
-f=[ #todo: date can also be: 1/2017/4/13 --> "2016-09-06Z"^^xsd:date
-              #lambda x: "wiki:Property-3ADct-3Adate  \"" + simple(x["Dct:date"][0]['raw']+'"^^xsd:gYear'),
-          # lambda y: codeTitle("wiki:Property-3ADct-3Acreator  ",y["Dct:creator"],False),
-          # lambda y: codeTitle("wiki:Property-3ADct-3Asubject  wiki:" ,y["Dct:subject"],True),
-          # lambda y: codeTitle("wiki:Property-Organization  ",y["Organization"],False),
-lambda y: codeTitle("wiki:Property-3ASupercontext  wiki:",y["Supercontext"],True),
-           lambda y: codeTitle("wiki:Property-3AFile_name  wiki:",y["File name"],True),
-lambda y: "wiki:Property-3AHyperlink  <"+y["Hyperlink"][0]+">",
-lambda y: "wiki:Property-3ASemantic_title  \"" + escapeQuote(y["Semantic title"][0]) + "\"",
-     lambda y: "wiki:Property-3AName  \"" + escapeQuote(y["Semantic title"][0]) + "\""]
-#doCommand(query,cmd,f)
+      """
+
+    f = [lambda x: codeTitle("rdf:type  wiki:", x["Category"], True).replace("Categorie", "Category"),
+         lambda y: codeTitle("wiki:Property-3ASupercontext  wiki:", y["Supercontext"], True),
+         lambda y: "wiki:Property-3ASemantic_title  \"" + escapeQuote(y["Semantic title"][0]) + "\"",
+         lambda y: "wiki:Property-3AName  \"" + escapeQuote(y["Semantic title"][0]) + "\""]
+
+    query = "[[Category:Light Context||Project||Projecten]]|?Semantic title|?Category=Category|?Supercontext|sort=Semantic title|order=asc|limit=10000"
+    # look for sparql-result:
+    # select ?p ?o where { <http://localhost:5555/wikis/hzportfolio/wiki/index.php/LC_00249> ?p ?o}
+    doCommand(query,cmd,f,listdata)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    query="[[Category:Resource Description]]|?Semantic title|?file name|?hyperlink|?Dct:creator|?Dct:date|?Organization|?Dct:subject|limit=10000"
+    cmd="""
+    <{0}> 
+      rdf:type  swivt:Subject;
+      rdf:type  wiki:Category-3AResource_Description;
+    [3]
+      rdfs:label  "{1}";
+      swivt:page  <{0}>;
+    [6]
+    [7]
+    [4]
+      wiki:Property-3ADisplay_title_of  "{1}";
+    [5]
+      wiki:Property-3ASelf  wiki:{2}
+      .
+    
+      """
+    f=[ #todo: date can also be: 1/2017/4/13 --> "2016-09-06Z"^^xsd:date
+                  #lambda x: "wiki:Property-3ADct-3Adate  \"" + simple(x["Dct:date"][0]['raw']+'"^^xsd:gYear'),
+              # lambda y: codeTitle("wiki:Property-3ADct-3Acreator  ",y["Dct:creator"],False),
+              # lambda y: codeTitle("wiki:Property-3ADct-3Asubject  wiki:" ,y["Dct:subject"],True),
+              # lambda y: codeTitle("wiki:Property-Organization  ",y["Organization"],False),
+    lambda y: codeTitle("wiki:Property-3ASupercontext  wiki:",y["Supercontext"],True),
+               lambda y: codeTitle("wiki:Property-3AFile_name  wiki:",y["File name"],True),
+    lambda y: "wiki:Property-3AHyperlink  <"+y["Hyperlink"][0]+">",
+    lambda y: "wiki:Property-3ASemantic_title  \"" + escapeQuote(y["Semantic title"][0]) + "\"",
+         lambda y: "wiki:Property-3AName  \"" + escapeQuote(y["Semantic title"][0]) + "\""]
+    doCommand(query,cmd,f,listdata)
+    afile = open('save.pkl', 'wb')
+    pickle.dump([datetime.datetime.utcnow(),listdata], afile)
+    afile.close()
 
 def getRecentChanges():
+    global method
+    method = "POST"
+
+    try:
+        afile = open('save.pkl', 'rb')
+        [prevdate,listdata]=pickle.load(afile)
+        afile.close()
+    except:
+        listdata={}
+        prevdate=datetime.datetime.utcnow() - datetime.timedelta(days=3*365)
+    print(prevdate)
     #https://www.projectenportfolio.nl/wiki/api.php?action=query&list=recentchanges&rcprop=title&rcstart=2017-11-24T00:00:00Z
-    import datetime
-    today = datetime.date.today()
-    print (today) #rcstart={0}T00:00:00Z&
-    url = (wikiurl + "/api.php?action=query&list=recentchanges&rcprop=title&rcstart={0}T00:00:00Z&format=json") .format(today) #&rclimit=500
+
+    today = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+    #print (today) #rcstart={0}T00:00:00Z&
+    #timestamp example: "2017-11-23T10:56:43Z"
+    #previous:&rcstart={0}T00:00:00Z
+    url = (wikiurl + "/api.php?action=query&list=recentchanges&rcprop=title|timestamp&rclimit=100&format=json") .format(today) #&rclimit=500
     print(url)
     r = requests.get(url)
     data = json.loads(r.text)
     # print(data)
     result=data["query"]["recentchanges"]
     urls=set()
+    maxdate=prevdate
     for item in result:
         title = item["title"]
+        #print(title)
+        if title in listdata:
+            #print("title in list")
+            continue
+        timestamp=item["timestamp"]
+        time=datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+        if time<prevdate:
+            continue
+        listdata.append(title)
+        if time>maxdate:
+            maxdate=time
+        #print(time)
         urls.add(title)
+        print("add:"+title)
+    if len(urls)==0:
+
+        global debug
+        debug=True
+        return
+    afile = open('save.pkl', 'wb')
+    pickle.dump([maxdate,listdata], afile)
+    afile.close()
     urls=list(urls)
     possiblePages={"Resource Description","Light Context"}
     pages=dict()
     print(len(urls))
-    totalNames=""
-    #todo: max of 19 items per time!?
+
+    #max of 19 items per time!?
     for item in urls:
         title=item
+        totalNames = ""
         totalNames+="|"+urllib.parse.quote_plus(title)
         #https://www.projectenportfolio.nl/wiki/api.php?action=query&titles=PR%20SSM%2000020&prop=categories|pageprops
-    totalNames = totalNames[1:]
-    titleurl=(wikiurl + "/api.php?action=query&titles={0}&prop=categories|pageprops&format=json") .format(totalNames)
-    print(titleurl)
-    r2 = requests.get(titleurl)
-    data = json.loads(r2.text)
-    pagesreturn=data["query"]["pages"]
-    for key in pagesreturn:
-        #print(title)
-        page=pagesreturn[key]
-        #print(page)
+        totalNames = totalNames[1:]
+        titleurl=(wikiurl + "/api.php?action=query&titles={0}&prop=categories|pageprops&format=json") .format(totalNames)
+        print(titleurl)
+        r2 = requests.get(titleurl)
+        data = json.loads(r2.text)
+        pagesreturn=data["query"]["pages"]
+        for key in pagesreturn:
+            #print(title)
+            page=pagesreturn[key]
+            #print(page)
 
-        #if not "title" in page:
-        #    print(page)
-        #    continue
-        title=page["title"]
-        mypage={}
-        mypage['id'] = title.replace(" ","_")
-        mypage['displaytitle']=title
-        for key in page:
-            try:
-                categories=page[key]["categories"]
-                cats=[]
-                for item in categories:
-                    cats.append(item["title"].replace("Category:","").replace("Categorie:",""))
+            #if not "title" in page:
+            #    print(page)
+            #    continue
+            title=page["title"]
+            mypage={}
+            mypage['id'] = title.replace(" ","_")
+            #todo: semantic title is not set when it is saved as resource; only when updated with properties......
+            mypage['displaytitle']=title
+            for key in page:
+                try:
+                    categories=page[key]["categories"]
+                    cats=[]
+                    for item in categories:
+                        cats.append(item["title"].replace("Category:","").replace("Categorie:",""))
+                    mypage["categories"]=cats
+                except:
+                    mypage["categories"] = []
+                try:
+                    pageprops=page[key]["pageprops"]
+                    mypage['displaytitle'] = pageprops['displaytitle']
+                except:
+                    pass
+            cats=set(mypage["categories"])
+            if len(cats-possiblePages)==0:
+                cats=list(possiblePages-cats)
                 mypage["categories"]=cats
-            except:
-                mypage["categories"] = []
-            try:
-                pageprops=page[key]["pageprops"]
-                mypage['displaytitle'] = pageprops['displaytitle']
-            except:
-                pass
-        cats=set(mypage["categories"])
-        if len(cats-possiblePages)==0:
-            cats=list(possiblePages-cats)
-            mypage["categories"]=cats
-            pages[key]=mypage
+                pages[key]=mypage
 
-            # &prop=revisions&rvprop=content
+                # &prop=revisions&rvprop=content
 
-    titleurl = (wikiurl + "/api.php?action=query&titles={0}&prop=revisions&rvprop=content&format=json").format(
-        totalNames)
-    r2 = requests.get(titleurl)
-    data = json.loads(r2.text)
-    contentJson = data["query"]["pages"]
-    #print(pages)
-    #todo: check if property hyperlink or file name is in page......... Check with content!?
-    #todo: can it be done with id? That is given by page-info!
-    for key in pages:
-        item=pages[key]
-        hyperlink = False
-        pagename = False
-        if "Resource Description" in item["categories"]:
-            try:
-                content=content["revisions"][0]['*']
-                hyperlink="|hyperlink=" in content.lower()
-                pagename = "|page name=" in content.lower()
-                #print(hyperlink,pagename)
-            except:
-                pass
-        cmd="""<{0}> 
-  rdf:type  swivt:Subject;
-  rdfs:label  "{1}";
-  swivt:page  <{0}>;
-  wiki:Property-3ADisplay_title_of  "{1}";
-  wiki:Property-3ASelf  wiki:{2};
-  wiki:Property-3ASemantic_title  "{3}";
-{4}{5}
-  .
+        titleurl = (wikiurl + "/api.php?action=query&titles={0}&prop=revisions&rvprop=content&format=json").format(
+            totalNames)
+        r2 = requests.get(titleurl)
+        data = json.loads(r2.text)
+        contentJson = data["query"]["pages"]
+        #print(pages)
+        for key in pages:
+            item=pages[key]
+            hyperlink = False
+            pagename = False
+            if "Resource Description" in item["categories"]:
+                try:
+                    content=content["revisions"][0]['*']
+                    hyperlink="|hyperlink=" in content.lower()
+                    pagename = "|page name=" in content.lower()
+                    #print(hyperlink,pagename)
+                except:
+                    pass
+            cmd="""<{0}> 
+      rdf:type  swivt:Subject;
+      rdfs:label  "{1}";
+      swivt:page  <{0}>;
+      wiki:Property-3ADisplay_title_of  "{1}";
+      wiki:Property-3ASelf  wiki:{2};
+      wiki:Property-3ASemantic_title  "{3}";
+    {4}{5}
+      .
+    
+      """
 
-  """
+            lc=item["id"] #url
+            self1=encodeUrl(lc)
+            label=escapeQuote(item['displaytitle'])
+            url=wiki+"/"+item["id"]
+            semanticTitle=label
+            cat="  rdf:type  wiki:Category-3A{0};\n"
+            cats=""
+            for c in item["categories"]:
+                cats+=cat.format(c.replace(" ", "_"))
+            extra=""
+            if hyperlink:
+                extra+="  wiki:Property-3AHyperlink  \"zomaar\";  \n"
+            if pagename:
+                extra+="  wiki:Property-3APage name  \"zomaar\";\n  "
+            formatted = cmd.format(url, label, lc, semanticTitle, cats,extra)
+            #print(formatted)
 
-        lc=item["id"] #url
-        self1=encodeUrl(lc)
-        label=escapeQuote(item['displaytitle'])
-        url=wiki+"/"+item["id"]
-        semanticTitle=label
-        cat="  rdf:type  wiki:Category-3A{0};\n"
-        cats=""
-        for c in item["categories"]:
-            cats+=cat.format(c.replace(" ", "_"))
-        extra=""
-        if hyperlink:
-            extra+="  wiki:Property-3AHyperlink  \"zomaar\";  \n"
-        if pagename:
-            extra+="  wiki:Property-3APage name  \"zomaar\";\n  "
-        formatted = cmd.format(url, label, lc, semanticTitle, cats,extra)
-        #print(formatted)
-
-        fh.write(formatted)
-
-getRecentChanges()
+            fh.write(formatted)
+try:
+    param=sys.argv[1]
+except:
+    param="dump"
+if param=="dump":
+    saveAllResources()
+else:
+    getRecentChanges()
 #rcstart="2017-11-21T16:18:06Z"
 fh.close()
 print("--- %s seconds ---" % (time.time() - start_time))
+#Use accessor.add(m) instead of putModel(m)
 if not debug:
-    command = '''curl -X PUT  -H Content-Type:text/turtle  -T data.ttl  -G http://localhost:{0}/hzportfolio/data'''.format(sparqlport)
+    command = '''curl -X {1}  -H Content-Type:text/turtle  -T data.ttl  -G http://localhost:{0}/hzportfolio/data'''.format(sparqlport,
+                                                                                                                           method)
     print(command)
     print(os.system(command))
 
